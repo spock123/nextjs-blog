0bf3818776783ee1123529672b303ba889c2acf6
Lars Rye Jeppesen <lars.jeppesen@gmail.com>
2022-02-17 19:17:51 +0100

    build: nextjs 12.1, tailwind 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 module.exports = {
   important: true,
+  prefix: '',
+  content: ['./src/**/*.{html,ts, scss, txs, vue,}'],
+  darkMode: 'class',
   purge: [
     './pages/**/*.js',
     './pages/**/*.tsx',
     './components/**/*.js'
-  ]
-  // ...
+  ],
+  plugins: [require('@tailwindcss/typography')]
+
 }
